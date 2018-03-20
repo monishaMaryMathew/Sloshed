4899009f3a2f1ccd79ff0b2aa511696f434850b5
ramonavaz25 <rvaz@ufl.edu>
2018-03-19 21:00:57 -0400

    cab Fragment commit 2

--- app/src/main/java/com/monisha/samples/sloshed/fragments/CabBookingFragment.java
+++ app/src/main/java/com/monisha/samples/sloshed/fragments/CabBookingFragment.java
@@ -3,22 +3,41 @@ package com.monisha.samples.sloshed.fragments;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.lyft.lyftbutton.LyftButton;
 import com.lyft.lyftbutton.RideParams;
 import com.lyft.lyftbutton.RideTypeEnum;
 import com.lyft.networking.ApiConfig;
+import com.lyft.networking.LyftApiFactory;
+import com.lyft.networking.apiObjects.RideType;
+import com.lyft.networking.apiObjects.RideTypesResponse;
+import com.lyft.networking.apis.LyftPublicApi;
 import com.monisha.samples.sloshed.R;
+import com.monisha.samples.sloshed.util.CurrentLocation;
 import com.uber.sdk.android.core.UberSdk;
 import com.uber.sdk.android.rides.RideParameters;
 import com.uber.sdk.android.rides.RideRequestButton;
@@ -27,7 +46,16 @@ import com.uber.sdk.rides.client.ServerTokenSession;
 import com.uber.sdk.rides.client.SessionConfiguration;
 import com.uber.sdk.android.rides.RideRequestButton;
 
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -43,12 +71,21 @@ public class CabBookingFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     RideRequestButton requestButton;
-    LyftButton lyftButton;
+    private static final int PERMISSION_REQUEST_CODE = 1336;
+    private static final long LOCATION_UPDATE_INTERVAL_MS = 15000;
+    private static final float LOCATION_MIN_DISPLACEMENT_METERS = 10;
+    private final Set<Call> callSet = new HashSet<>();
+    private Spinner rideTypeSpinner;
+    private ArrayAdapter<String> adapter;
+    private LyftPublicApi lyftPublicApi;
+    private LyftButton lyftButton;
+    private GoogleApiClient googleApiClient;
+    private double currentLat = 0.0;
+    private double currentLng = 0.0;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
     private OnFragmentInteractionListener mListener;
 
 
@@ -91,6 +128,7 @@ public class CabBookingFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cab_booking, container, false);
         lyftButton = (LyftButton) view.findViewById(R.id.lyft_button);
+        rideTypeSpinner = (Spinner) view.findViewById(R.id.ride_type_dropdown);
         if (ContextCompat.checkSelfPermission(this.getContext(),
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -184,28 +222,6 @@ public class CabBookingFragment extends Fragment {
 
     }
 
-    private void lyftCall() {
-
-
-        ApiConfig apiConfig = new ApiConfig.Builder()
-                .setClientId("5HOldz5wTBSf")
-                .setClientToken("DX9b/VXqyDZup+icMV0km12nJs4vkkSTfXj6qh8+tO8QWSCJ6GlvyztezDVHEQ+ucTPNZDUK9i0diOY6Nhd/cgn+lDlPIAHtrJ0sW7VSBe5DhkG0PpYA/pY=")
-                .build();
-       // View view = this.getView();
-
-        lyftButton.setApiConfig(apiConfig);
-
-        RideParams.Builder rideParamsBuilder = new RideParams.Builder()
-                .setPickupLocation(37.7766048, -122.3943629)
-                .setDropoffLocation(37.759234, -122.4135125);
-        rideParamsBuilder.setRideTypeEnum(RideTypeEnum.CLASSIC);
-
-        lyftButton.setRideParams(rideParamsBuilder.build());
-        lyftButton.load();
-
-
-    }
-
     private void rideRequest() {
         RideParameters rideParams = new RideParameters.Builder()
                 // Optional product_id from /v1/products endpoint (e.g. UberX). If not provided, most cost-efficient product will be used
@@ -221,5 +237,157 @@ public class CabBookingFragment extends Fragment {
 
 
     }
+
+    private void lyftCall() {
+
+        ApiConfig apiConfig = new ApiConfig.Builder()
+                .setClientId("5HOldz5wTBSf")
+                .setClientToken("DX9b/VXqyDZup+icMV0km12nJs4vkkSTfXj6qh8+tO8QWSCJ6GlvyztezDVHEQ+ucTPNZDUK9i0diOY6Nhd/cgn+lDlPIAHtrJ0sW7VSBe5DhkG0PpYA/pY=")
+                .build();
+
+        lyftButton.setApiConfig(apiConfig);
+        lyftPublicApi = new LyftApiFactory(apiConfig).getLyftPublicApi();
+        initializeRideTypeSpinner();
+        setupLocationApi();
+
+
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        googleApiClient.disconnect();
+        for (Call call : callSet) {
+            call.cancel();
+        }
+        callSet.clear();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE && isLocationPermissionGranted()) {
+            googleApiClient.connect();
+        }
+    }
+
+    private void initializeRideTypeSpinner() {
+
+        adapter = new ArrayAdapter<>(this.getContext(), android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.add(getResources().getString(R.string.loading_text));
+        rideTypeSpinner.setAdapter(adapter);
+        rideTypeSpinner.setSelection(0, false);
+        rideTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                refreshButton((String) rideTypeSpinner.getSelectedItem());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+    }
+
+    @SuppressWarnings({"MissingPermission"})
+    private void setupLocationApi() {
+        googleApiClient = new GoogleApiClient.Builder(this.getActivity())
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(@Nullable Bundle bundle) {
+                        LocationRequest locationRequest = LocationRequest.create();
+                        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                        locationRequest.setInterval(LOCATION_UPDATE_INTERVAL_MS);
+                        locationRequest.setSmallestDisplacement(LOCATION_MIN_DISPLACEMENT_METERS);
+                        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest,
+                                new com.google.android.gms.location.LocationListener() {
+                                    @Override
+                                    public void onLocationChanged(Location location) {
+                                        currentLat = location.getLatitude();
+                                        currentLng = location.getLongitude();
+                                        getRideTypesAtCurrentLocation();
+                                    }
+                                });
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+                    }
+                })
+                .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                    }
+                })
+                .build();
+
+        if (isLocationPermissionGranted()) {
+            googleApiClient.connect();
+        } else {
+            ActivityCompat.requestPermissions(this.getActivity(), new String[] { Manifest.permission.ACCESS_FINE_LOCATION }, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private void getRideTypesAtCurrentLocation() {
+
+        Call<RideTypesResponse> call = lyftPublicApi.getRidetypes(currentLat, currentLng, RideTypeEnum.ALL.toString());
+        callSet.add(call);
+        call.enqueue(new Callback<RideTypesResponse>() {
+            @Override
+            public void onResponse(Call<RideTypesResponse> call, Response<RideTypesResponse> response) {
+                callSet.remove(call);
+                adapter.clear();
+                RideTypesResponse rideTypesResponse = response.body();
+                if (isLyftAvailable(rideTypesResponse)) {
+                    List<RideType> rideTypes = rideTypesResponse.ride_types;
+                    for (RideType rideType : rideTypes) {
+                        adapter.add(rideType.display_name);
+                    }
+                    rideTypeSpinner.setSelection(adapter.getPosition(RideTypeEnum.CLASSIC.getDisplayName()));
+                } else {
+                    adapter.add("LYFT N/A");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RideTypesResponse> call, Throwable t) {
+                callSet.remove(call);
+            }
+        });
+    }
+
+    private boolean isLocationPermissionGranted() {
+        return ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void refreshButton(String selectedRideType) {
+        RideTypeEnum rideTypeEnum = RideTypeEnum.CLASSIC;
+
+        for (RideTypeEnum rte : RideTypeEnum.values()) {
+            if (rte.getDisplayName().equals(selectedRideType)) {
+                rideTypeEnum = rte;
+            }
+        }
+
+        RideParams.Builder rideParamsBuilder = new RideParams.Builder()
+                .setPickupLocation(currentLat, currentLng);
+                //.setDropoffLocation(37.759234, -122.4135125);
+        rideParamsBuilder.setRideTypeEnum(rideTypeEnum);
+
+        lyftButton.setRideParams(rideParamsBuilder.build());
+        lyftButton.load();
+    }
+
+    private static boolean isLyftAvailable(RideTypesResponse rideTypesResponse) {
+        return rideTypesResponse != null && rideTypesResponse.ride_types != null && !rideTypesResponse.ride_types.isEmpty();
+    }
+
+
+
+
+
+
 
 }
