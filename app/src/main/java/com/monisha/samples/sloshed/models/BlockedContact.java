0b090769558bd9b31b3c636b941004e4bb8dbbcd
ramonavaz25 <rvaz@ufl.edu>
2018-03-25 00:36:40 -0400

    cab Fragment commit 2

---
+++ app/src/main/java/com/monisha/samples/sloshed/models/BlockedContact.java
@@ -0,0 +1,15 @@
+package com.monisha.samples.sloshed.models;
+
+/**
+ * Created by Monisha on 3/10/2018.
+ */
+
+public class BlockedContact {
+    public String contactName;
+    public String phoneNumber;
+
+    public BlockedContact(String contactName, String phoneNumber) {
+        this.contactName = contactName;
+        this.phoneNumber = phoneNumber;
+    }
+}
